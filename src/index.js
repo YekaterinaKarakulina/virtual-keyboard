a71558cae68a7e516d4a6ca42455319dd6454843
Yekaterina Karakulina <ekaterina.jj@gmail.com>
2020-04-02 13:13:37 +0400

    feat: impliment generateKeys() method

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import {Keyboard} from './js/Keyboard';
 import {Textarea} from './js/Textarea';
 
 const keyboard_eng = [
-    '~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace',
-    'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Delete',
-    'CapsLk', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter',
-    'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', 'Up',
-    'Ctrl', 'Alt', 'Backspace', 'Alt', 'Ctrl', 'Left', 'Down', 'Right'
+    ['~', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'Delete'],
+    ['CapsLk', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\\', 'Enter'],
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', 'Up'],
+    ['Ctrl', 'Alt', 'Space', 'Alt', 'Ctrl', 'Left', 'Down', 'Right']
 ];
 
-
+// '\\',\
 window.onload = function() {
     console.log("hello");
 
@@ -22,10 +22,7 @@ window.onload = function() {
     keyboard.generateKeyboardContainer();
 
     //create keys
-    for(let i=0; i<keyboard_eng.length; i++)
-    {
-        keyboard.generateKey(keyboard_eng[i]);
-    }
+    keyboard.generateKeys(keyboard_eng);
     
     
 }
--- src/js/Keyboard.js
+++ src/js/Keyboard.js
@@ -13,13 +13,25 @@ generateKeyboardContainer() {
     document.body.append(keyboardContainer);
 }
 
-generateKey(keyName) {
-    let key = document.createElement('button');
-    key.classList.add(keyName);
-    key.classList.add('keyboard__key');
-    key.innerHTML = keyName;
-    document.querySelector('.keyboard__keys').append(key);
+generateKeys(keysArray) {
+    for(let i=0; i<keysArray.length; i++) {
+        for(let j=0; j<keysArray[i].length; j++) {
+            let key = document.createElement('button');
+            key.classList.add('keyboard__key');
+            key.innerHTML = keysArray[i][j];
+            document.querySelector('.keyboard__keys').append(key);
+        }
+        document.querySelector('.keyboard__keys').append(document.createElement('br')); 
+    }
 }
+
+// generateKey(keyName) {
+//     let key = document.createElement('button');
+//     key.classList.add(keyName);
+//     key.classList.add('keyboard__key');
+//     key.innerHTML = keyName;
+//     document.querySelector('.keyboard__keys').append(key);
+// }
 
 
 }
